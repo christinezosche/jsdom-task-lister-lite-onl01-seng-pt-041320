8837e4ff01b2488c7c729bd4e1e0246d7665141b
christinezosche <christine.zosche@gmail.com>
2020-10-27 09:57:53 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const tasks = document.getElementById('tasks');
+  const form1 = document.getElementById('create-task-form');
+  const form2 = document.getElementById('delete-task-form');
+  
+  
+  form1.addEventListener('submit', function(event) {
+    let li = document.createElement('li');
+    let data = document.getElementById('new-task-description').value
+    let node = document.createTextNode(data)
+    li.appendChild(node)
+    tasks.appendChild(li)
+    event.preventDefault();
+  });
+
+  form2.addEventListener('submit', function(event) {
+    tasks.querySelectorAll("li").remove();
+    event.preventDefault();
+  });
+
 });
+
